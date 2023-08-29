abdb9a0bcb8927fb2b052f399ebc51fec5793fdb
Wandati <Petersimon801@gmail.com>
2023-08-29 13:49:34 +0300

    update

--- datastructures.py
+++ datastructures.py
@@ -23,14 +23,20 @@ print(is_balanced(expression1)) # Output: True
 print(is_balanced(expression2)) # Output: False
 # Solution 2
 # Sequences (Lists/Tuples)
+from timeit import default_timer
 def remove_duplicates(sequence):
+    
     new_set = set()
-    sequence_list = []
     for item in sequence:
-        if item not in new_set:
-            sequence_list.append(item)
-            new_set.add(item)
-    return sequence_list
+        new_set.add(item)
+    return list(new_set)
+    
+    # sequence_list = []
+    # for item in sequence:
+    #     if item not in new_set:
+    #         sequence_list.append(item)
+    #         new_set.add(item)
+    # return sequence_list
    
 input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
 result = remove_duplicates(input_sequence)
